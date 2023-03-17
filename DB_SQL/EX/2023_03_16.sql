c6f1dbded7d1cc79849acf472b0cbdbcdcbcad8d
Headh1 <a.design804@gmail.com>
2023-03-17 12:01:34 +0900

    2023.03.17

--- DB_SQL/EX/2023_03_16.sql
+++ DB_SQL/EX/2023_03_16.sql
@@ -29,10 +29,12 @@
 -- ORDER BY emp.emp_no;
 
 -- 5. 현재 월급의 상위 10위까지 사원의 사번, 풀네임 ,월급을 출력
--- SELECT RANK()over(ORDER BY sa.salary ASC) rank_sal , sa.emp_no, concat(emp.last_name,' ' ,emp.first_name) ,sa.salary , sa.to_date
+-- SELECT RANK()over(ORDER BY sa.salary deSC) rank_sal , sa.emp_no, concat(emp.last_name,' ' ,emp.first_name) ,sa.salary , sa.to_date
 -- FROM salaries sa 
 -- 	INNER JOIN employees emp
--- 		on sa.emp_no=emp.emp_no limit 10;
+-- 		on sa.emp_no=emp.emp_no
+-- WHERE sa.to_date='9999-01-01'
+-- limit 10;
 		
 -- 6. 각 부서의 부서장의 부서명, 풀네임, 입사일  출력
 -- SELECT de.dept_name , concat(emp.last_name,' ',emp.first_name) , emp.hire_date , dm.to_date
@@ -51,11 +53,11 @@
 -- ORDER BY dep.dept_no asc;
 
 -- 7.현재 직책이 스태프인 사원의 현재 평균 월급 출력
--- SELECT ti.title , avg(sa.salary)
+-- SELECT ti.title ,avg(sa.salary)
 -- FROM titles ti 
 -- 	INNER JOIN salaries sa
 -- 		ON ti.emp_no = sa.emp_no
--- WHERE title='Staff' and sa.to_date = '9999-01-01';
+-- WHERE ti.title='Staff' and sa.to_date = '9999-01-01' and ti.to_date = '9999-01-01';
 
 -- 8.부서장직을 역임했던 모든 사원의 풀네임과 입사일, 사번, 부서번호를 출력
 -- SELECT concat(emp.last_name,' ' ,emp.first_name) , emp.hire_date , emp.emp_no ,dm.dept_no
@@ -64,13 +66,13 @@
 -- 		ON emp.emp_no = dm.emp_no;
 -- 
 -- 9.현재 각 직급별 평균월급 중 60,000이상인 직급의 직급명, 평균월급(정수)를 내림차순 출력
--- SELECT ti.title , ti.to_date, cast(AVG(sa.salary) AS int) AS avg_sal
+-- SELECT ti.title , ti.to_date, cast(AVG(sa.salary) as int) AS a_s
 -- FROM titles ti
 -- 	inner JOIN salaries sa 
 -- 		ON ti.emp_no = sa.emp_no
 -- WHERE ti.to_date='9999-01-01' and sa.to_date = '9999-01-01'
--- GROUP BY ti.title having avg_sal>=60000
--- ORDER BY avg_sal DESC;
+-- GROUP BY ti.title having a_s>=60000
+-- ORDER BY a_s DESC;
 
 -- SELECT ti.title , ti.to_date, TRUNCATE(AVG(sa.salary),0)
 -- FROM titles ti
@@ -80,13 +82,23 @@
 -- GROUP BY ti.title having AVG(sa.salary)>=60000
 -- ORDER BY AVG(sa.salary) DESC;
 
+-- SELECT ti.title , ti.to_date, cast(AVG(sa.salary) AS int) AS a_s
+-- FROM titles ti
+-- 	inner JOIN salaries sa 
+-- 		ON ti.to_date = sa.to_date
+-- WHERE ti.to_date='9999-01-01'
+-- GROUP BY ti.title having a_s>=60000
+-- ORDER BY a_s DESC;
+
 -- 10. 성별이 여자인 사원들의 직급별 사원수를 출력
--- SELECT ti.title , count(ti.title)
--- FROM employees em
--- 	INNER JOIN titles ti 
--- 		ON em.emp_no = ti.emp_no
--- WHERE em.gender = 'F'
--- GROUP BY title;
+SELECT ti.title , COUNT(*)
+FROM employees em
+	INNER JOIN titles ti 
+		ON em.emp_no = ti.emp_no
+WHERE em.gender = 'F' AND ti.to_date = '9999-01-01'
+GROUP BY title;
+
+
 
 
 
