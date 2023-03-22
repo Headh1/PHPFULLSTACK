2f50a5711ffd7cddaffbf09bb7652582501a99e0
Headh1 <a.design804@gmail.com>
2023-03-22 13:52:34 +0900

    2023.03.22

---
+++ PHP/STUDY/basic.php
@@ -0,0 +1,29 @@
+<?php
+
+    print"Hello world!\n";
+    print("Hello world!\n");
+
+    echo"Hello world!\n";
+
+    $test_num = 1;
+    echo $test_num;
+
+    // $age =28;
+    // echo "\n나이는 $age 입니다";
+
+    // $sum = 233+453+56;
+    // echo "\n합은 $sum 입니다";
+
+    echo"\n\n점심메뉴\n탕수육 8,000\n짜장면 6,000\n짬뽕 6,000";
+
+    $a ="탕수육 8,000";
+    $b = "짜장면 6,000";
+    $c = "짬뽕";
+    $blank = " ";
+    $line = "\n";
+    $price_a = "8,000";
+    $price_b = "6,000";
+
+    echo"\n\n점심메뉴 \n$a";
+    echo $line.$c.$blank.$price_b;
+?>
---
+++ PHP/STUDY/operator.php
@@ -0,0 +1,43 @@
+<?php
+// echo "덧셈 ",1+0;
+// echo " \n뺄셈 ",3-1;
+// echo " \n곱셈 ",1*3;
+// echo " \n나눗셈 ",8/2;
+// echo " \n나머지 ",12%7;
+// echo " \n연산순서 ",10-2*2;
+
+// $num1 = 1;
+// $num2 = 1;
+
+// echo "\n" , ++$num1 ;
+// echo "\n", --$num2;
+
+// echo "\n", $num1++ , $num1;
+
+// echo "\n\n";
+
+// $num = 1;
+// $num1 = $num+1;
+// $num += 1;
+
+// echo $num1, " ",$num += 1;
+
+// $tng = 10;
+// $tng2 = $tng+10;
+// $tng3 = $tng/5;
+// $tng4 = $tng-4;
+// $tng5 = $tng%7;
+// $tng6 = $tng*3;
+// echo $tng2,"\n",$tng3,"\n",$tng4,"\n",$tng5,"\n",$tng6;
+
+// $tng_n = 10;
+// echo "\n\n", $tng_n += 10;
+// echo "\n", $tng_n /= 5;
+// echo "\n", $tng_n -= 4;
+// echo "\n", $tng_n %= 7;
+// echo "\n", $tng_n *= 3;
+
+// echo "\n\n",$tng_n;
+
+
+?>
