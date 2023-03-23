9718309878d7871e25edc3845dd9c149c512ed39
Headh1 <a.design804@gmail.com>
2023-03-23 15:41:48 +0900

    2023.03.23

--- PHP/STUDY/while.php
+++ PHP/STUDY/while.php
@@ -1,3 +1,21 @@
 <?php
+// $i= 1;
+// $j= 2;
+// while($i<10)
+// {
+//     echo "$j*$i"."=".$i*$j."\n";
+//     $i++;
+
+// }
+
+$i=0;
+do{
+    echo $i,"do";
+}
+while($i===1);
+
+while($i===1){
+    echo $i, "while";
+}
 
 ?>
