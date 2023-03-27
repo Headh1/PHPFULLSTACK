4d84f5d0ae76483bf3f3d353e54ffd41156237cf
Headh1 <a.design804@gmail.com>
2023-03-27 13:13:39 +0900

    2023.03.27

---
+++ PHP/STUDY/array.php
@@ -0,0 +1,44 @@
+<?php
+// $number= array("zero","one","two");
+// echo $number[0];
+
+// $zero="zero";
+// $one="one";
+// $two="two";
+
+// $number= array($zero,$one,$two);
+// echo $number[1];
+
+// $number2 = array($number[0],$number[1],$number[2]);
+// echo $number2[0];
+
+
+// $week= array("sun","mon","tue","wed");
+
+// $week2 = array($week[1],$week[3],$week[0],$week[2]);
+// echo $week2[0];
+// echo $week2[1];
+// echo $week2[2];
+// echo $week2[3];
+
+// $ar_m = array("abc",1,3.14,'a');
+// echo $ar_m[1];
+
+// $ar_as = array("key1"=>"val1", "key2"=>"val2");
+// echo $ar_as["key1"]; 
+
+// $ar_te = array(
+//             array(1,2,3,4) 
+//             ,array(5,6,7,8)
+//             ,array(
+//                 array(9,10,11)
+//             )
+//         );
+// echo $ar_te[1][0];
+// $ar_te_3 =array(array(9,10,11));
+
+$week= array("sun","mon","del","tue","wed");
+unset($week[2]);
+print_r($week);
+
+?>
